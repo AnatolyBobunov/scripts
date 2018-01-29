bcb7368389dd9ac48a58352e96b83346846eca64
Anatoly <dev.bobunov@gmail.com>
2018-01-30 00:51:23 +0400

    added new scripts - delete_logs

---
+++ delete_logs/delete_logs.py
@@ -0,0 +1,3 @@
+# delete logs and garbage files form in specific folders. Trigger file
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
---
+++ delete_logs/main_delete.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+'''
+Пробегаемся по папкам и если встречаем триггер файл (delete_logs.py),
+то удаляем все (файлы и папки) в папке с ним. 
+'''
+
+import os, shutil
+
+#значение исходной папки для рекурсивного сканирования
+dir = os.path.expanduser('/home/anatoly.bobunov/SF_Co') 
+
+
+def delete_logs(folder_dir):
+    '''
+    На входе получаем адрес папки с триггерным файлом.
+    Удаляем в папке все файлы и папки, кроме тех что оканчиваются на '.py','.py~','.idea'
+    доп фильтр можно добавить по собственному желанию
+    '''
+    for filename in os.listdir(folder_dir):       
+        if filename.endswith(('.py','.py~','.idea')) is False:
+            file = os.path.join(folder_dir, filename)
+            try:
+                if os.path.isfile(file):
+                    print(file + 'DELETED')
+                    os.unlink(file)
+                elif os.path.isdir(file): 
+                    print(file + ' DELETED')
+                    shutil.rmtree(file)
+            except Exception as e:
+                print(e)
+
+
+filelist = list(os.walk(dir))
+
+for root, dirs, files in filelist:
+    '''
+    Пробегает по всем подпапкам, если находим delete_log.py 
+    то возвращаем полный путь до этой папки
+    '''
+    for name in files:
+        if name == 'delete_logs.py':
+            folder_dir = os.path.join(root)
+            delete_logs(folder_dir)
+
+
+print('Delete is completed')
